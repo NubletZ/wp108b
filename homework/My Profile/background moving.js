7932245cb83932e3257920c5deb8b19a1f75df2d
NubletZ <wpandita2001@gmail.com>
2020-04-05 00:40:42 +0800

    modify My Profile's contents

--- homework/My Profile/background moving.js
+++
@@ -1,4 +0,0 @@
-$(window).scroll(function() {
-    var x = $(this).scrollTop();
-    $('#main').css('background-position', '100% ' + parseInt(-x / 1) + 'px' + ', 0% ' + parseInt(-x / 2) + 'px, center top');
-});
